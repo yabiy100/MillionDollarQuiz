1b66e822037e7031e3aa485d5563578194b01471
yasin <yasin-yb@hotmail.de>
2023-12-23 19:22:46 +0300

    -delet unnesisarry backend code from geoQuiz

--- app/src/main/java/com/mobil/android/milliondollarquiz/MainMenü.java
+++ app/src/main/java/com/mobil/android/milliondollarquiz/MainMenü.java
@@ -16,23 +16,7 @@ public class MainMenü extends AppCompatActivity {
     private static final String KEY_INDEX = "index";
     private static final String KEY_KOPYA = "kopya";
     private static final int REQUEST_CODE_CHEAT = 0;
-
-    private Button mTrueButton;
-    private Button mFalseButton;
-    private Button mNextButton;
     private Button mNewGameButton;
-    private TextView mQuestionTextView;
-
-    private Question[] mQuestionBank = new Question[] {
-            new Question(R.string.question_australia, true),
-            new Question(R.string.question_oceans, true),
-            new Question(R.string.question_mideast, false),
-            new Question(R.string.question_africa, false),
-            new Question(R.string.question_americas, true),
-            new Question(R.string.question_asia, true),
-    };
-
-    private int mCurrentIndex,kopyaIndex = -1;
 
 
     @Override
@@ -41,67 +25,14 @@ public class MainMenü extends AppCompatActivity {
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.mainmenu);
 
-        if (savedInstanceState != null) {
-            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
-            kopyaIndex=savedInstanceState.getInt(KEY_KOPYA, 0);
-        }
-
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(true);
-            }
-        });
-
-        mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(false);
-            }
-        });
-
-        mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-
-                updateQuestion();
-            }
-        });
-
         mNewGameButton = (Button) findViewById(R.id.new_game);
         mNewGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-                Intent intent = Game.newIntent(MainMenü.this, answerIsTrue);
-                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+                Intent intent = new Intent(MainMenü.this, Game.class);
+                startActivity(intent);
             }
         });
-
-        updateQuestion();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != Activity.RESULT_OK) {
-            return;
-        }
-
-        if (requestCode == REQUEST_CODE_CHEAT) {
-            if (data == null) {
-                return;
-            }
-            if (Game.wasAnswerShown(data)) {
-                kopyaIndex=mCurrentIndex;
-            }
-
-        }
     }
 
     @Override
@@ -126,8 +57,6 @@ public class MainMenü extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
-        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-        savedInstanceState.putInt(KEY_KOPYA, kopyaIndex);
     }
 
     @Override
@@ -140,29 +69,5 @@ public class MainMenü extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy() called");
-    }
-
-    private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
-    }
-
-    private void checkAnswer(boolean userPressedTrue) {
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-
-        int messageResId = 0;
-
-        if (kopyaIndex==mCurrentIndex) {
-            messageResId = R.string.judgment_toast;
-        } else {
-            if (userPressedTrue == answerIsTrue) {
-                messageResId = R.string.correct_toast;
-            } else {
-                messageResId = R.string.incorrect_toast;
-            }
-        }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-                .show();
     }
 }
