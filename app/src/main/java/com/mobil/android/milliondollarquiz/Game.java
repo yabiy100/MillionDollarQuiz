2be4587641e5b936100675e69f693094beddcda5
yasin <yasin-yb@hotmail.de>
2023-12-23 22:47:20 +0300

    -clean up game -create questions and there awnsers

--- app/src/main/java/com/mobil/android/milliondollarquiz/Game.java
+++ app/src/main/java/com/mobil/android/milliondollarquiz/Game.java
@@ -13,46 +13,22 @@ import java.util.Random;
 
 public class Game extends AppCompatActivity {
     private int numberQuestion =1;
+    private static final String KEY_NUMBER_QUESTION = "numberQuestion";
 
     //first element of moneyLevels array is empty because question numbers start with 1
     private final String[] moneyLevels = {
             "","$50","$100","$200","$300","$500","$1000","$2000","$4000","$8000","$16000","$32000",
             "$64000","$125000","$500000","$1000000"};
-
     private String[] questions;
-    private int randomNumber;
-    private static final String EXTRA_ANSWER_IS_TRUE =
-            "com.bignerdranch.android.MillionDollarQuiz.answer_is_true";
-    private static final String GAME_IS_OVER =
-            "com.mobil.android.MillionDollarQuiz.game_is_over";
+    private int randomNumber = new Random().nextInt(50) + 0;
+    private static final String KEY_RANDOM_NUMBER = "randomNumber";
     private int REQUEST_CODE_GAME_OVER = 0;
-    private static final String EXTRA_ANSWER_SHOWN =
-            "com.bignerdranch.android.geoquiz.answer_shown";
-    private static final String KEY_INDEX = "kopya";
-
-    private boolean mAnswerIsTrue;
-    private boolean isAnswerShown=false;
-
-    private TextView mAnswerTextView;
     private TextView mMoneyLevelTextView;
     private TextView mQuestionTextView;
-    private Button mShowAnswerButton;
     private Button mAnswerAButton;
     private Button mAnswerBButton;
     private Button mAnswerCButton;
     private Button mAnswerDButton;
-
-
-    //old intent
-    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
-        Intent intent = new Intent(packageContext, Game.class);
-        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
-        return intent;
-    }
-
-    public static boolean wasAnswerShown(Intent result) {
-        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,28 +36,10 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.game);
 
         if (savedInstanceState != null) {
-            isAnswerShown = savedInstanceState.getBoolean(KEY_INDEX, false);
-            setAnswerShownResult(isAnswerShown);
+            numberQuestion = savedInstanceState.getInt(KEY_NUMBER_QUESTION, 0);
+            randomNumber = savedInstanceState.getInt(KEY_RANDOM_NUMBER, 0);
         }
 
-        /*
-        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
-
-        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
-
-
-        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
-        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mAnswerIsTrue) {
-                    mAnswerTextView.setText(R.string.true_button);
-                } else {
-                    mAnswerTextView.setText(R.string.false_button);
-                }
-                setAnswerShownResult(true);
-            }
-        });*/
         questions = getResources().getStringArray(R.array.question);
 
         mMoneyLevelTextView = (TextView) findViewById(R.id.money_level);
@@ -93,7 +51,6 @@ public class Game extends AppCompatActivity {
         mAnswerCButton = (Button) findViewById(R.id.c);
         mAnswerDButton = (Button) findViewById(R.id.d);
 
-        randomNumber = new Random().nextInt(6) + 0;
         String nextQuestion = questions[randomNumber];
         mQuestionTextView.setText(nextQuestion);
 
@@ -103,14 +60,12 @@ public class Game extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkClicked(correctAnswers, 'a');
-                //setAnswerShownResult(true);
             }
         });
         mAnswerBButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkClicked(correctAnswers, 'b');
-                //setAnswerShownResult(true);
             }
         });
 
@@ -118,14 +73,12 @@ public class Game extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkClicked(correctAnswers, 'c');
-                //setAnswerShownResult(true);
             }
         });
         mAnswerDButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkClicked(correctAnswers, 'd');
-                //setAnswerShownResult(true);
             }
         });
     }
@@ -137,7 +90,7 @@ public class Game extends AppCompatActivity {
             numberQuestion++;
             mMoneyLevelTextView.setText(moneyLevels[numberQuestion]);
 
-            randomNumber = new Random().nextInt(6) + 0;
+            randomNumber = new Random().nextInt(50) + 0;
             String nextQuestion = questions[randomNumber];
             mQuestionTextView.setText(nextQuestion);
             messageResourceId = R.string.correct_toast;
@@ -153,14 +106,7 @@ public class Game extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-
-        savedInstanceState.putBoolean(KEY_INDEX, isAnswerShown);
-    }
-
-    private void setAnswerShownResult(boolean isAnswerShown) {
-        this.isAnswerShown=isAnswerShown;
-        Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
-        setResult(RESULT_OK, data);
+        savedInstanceState.putInt(KEY_NUMBER_QUESTION, numberQuestion);
+        savedInstanceState.putInt(KEY_RANDOM_NUMBER, randomNumber);
     }
 }
