d5fe6ac7ab5c63b894f052a0e63372add80218d8
ilker <ilkerdikme94@gmail.com>
2023-12-24 21:17:06 +0300

    -cleaned up unnecessary files and code

--- app/src/main/java/com/mobil/android/milliondollarquiz/MainMenü.java
+++ app/src/main/java/com/mobil/android/milliondollarquiz/MainMenü.java
@@ -12,19 +12,13 @@ import android.widget.Toast;
 
 public class MainMenü extends AppCompatActivity {
 
-    private static final String TAG = "QuizActivity";
-    private static final String KEY_INDEX = "index";
-    private static final String KEY_KOPYA = "kopya";
-    private static final int REQUEST_CODE_CHEAT = 0;
     private Button mNewGameButton;
     private Button mHighScoreButton;
-    private Button mStatisticsButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.mainmenu);
 
         mNewGameButton = (Button) findViewById(R.id.new_game);
@@ -48,36 +42,30 @@ public class MainMenü extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart() called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume() called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause() called");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG, "onSaveInstanceState");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop() called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
     }
 }
